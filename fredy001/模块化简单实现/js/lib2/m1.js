6ddffb0d6b98a0ed23999fda83429e75b7d03e3a
guowei.dong <dongguowei@caimiao.com>
2015-07-22 15:26:12 +0800

    add module

---
+++ fredy001/模块化简单实现/js/lib2/m1.js
@@ -0,0 +1,14 @@
+/**
+ * Created by guowei.dong on 2015/7/22.
+ */
+define(function(){
+
+    return {
+
+        getName:function(){
+            alert('zhangsan2');
+        }
+
+    }
+
+});
---
+++ fredy001/模块化简单实现/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * Created by guowei.dong on 2015/7/22.
+ */
+require.config({
+    baseUrl: "js/",
+    paths: {
+        m1: "lib/m1",
+        m21: "lib2/m1"
+    }
+});
+require(['m1', 'lib2/m1'], function (m1, m2) {
+    m1.getName();
+    m2.getName();
+});
