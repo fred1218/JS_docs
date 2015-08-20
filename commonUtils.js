2bfee9edf33e9af6b5a4cd50e17629ab8cbfbe4a
guowei.dong <donggw2030521@qq.com>
2015-08-20 18:05:35 +0800

    修改规范文档

--- commonUtils.js
+++ commonUtils.js
@@ -1,7 +1,31 @@
 /**
  * 字符数组等操作 ajax loading dialog 时间操作 正则验证
+ * date array Math list map(immutable),loadsh&underscore,es5 API,reduce,string
  * Created by guowei.dong on 2015/8/19.
  */
 define(['jquery', 'underscore'], function ($, _) {
+    function browser(){}
+    function log(){
+        console.debug();
+    }
+    function dateAbt() {
+        var d = new Date;
+        var d2=new Date('2011-11-11 23:59:59');
+        console.debug(d2.toTimeString(),d2.toDateString(),d2.toLocaleString());//本时区
+        console.debug(d2.toGMTString(),d2.toISOString(),d2.toUTCString());//格林威治，，协调世界时
+
+        log(d.getFullYear(), 1);
+        log(d.getMonth() + 1);
+        log(d.getDate())
+        log(d.getHours())
+        log(d.getMinutes())
+        log(d.getMilliseconds())
+        log(d.getTime());
+        log(d.toLocaleDateString())
+        log(d.toLocaleString());
+        document.write(d.toLocaleString())
+        log(d.toLocaleTimeString());
+    }
+
 
 });
