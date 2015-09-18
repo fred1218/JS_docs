2ca0f5a3d73e74d99179ffd16ef264725c57ec7d
guowei.dong <donggw2030521@qq.com>
2015-09-18 12:41:12 +0800

    u

--- 模块化测试/main.js
+++ 模块化测试/main.js
@@ -3,7 +3,8 @@ require.config({
         aavvw: 'aa',
         woshibb: 'bb',
         jquery:'jquery',
-        ud:"underscore"
+        ud:"underscore",
+        clz:"testClass"
     },
     urlArgs: "version=20150805"
 
@@ -11,10 +12,11 @@ require.config({
 /**
  * bb是define，就得返回一个对象，因为涉及到闭包,否则无法用到里面的方法
  */
-require(['aavvw', 'woshibb'], function (undefined, bb) {
+require(['aavvw', 'woshibb','clz'], function (undefined, bb,clz) {
     a();
     a1();
     bb.a();
     bb.a1();
-
+var aaa=new clz();
+    console.debug(aaa.name);
 })
---
+++ 模块化测试/testClass.js
@@ -0,0 +1,10 @@
+/**
+ * Created by guowei.dong on 2015/9/18.
+ */
+define(function () {
+    var A = function () {
+        this.name = "zhangsanXXX";
+    }
+
+    return A;
+})
